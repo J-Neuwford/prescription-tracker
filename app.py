6105a0eb8269bf1672451baa5732595499a973ab
Josh Neuwford <116881112+J-Gosling@users.noreply.github.com>
2024-08-23 14:08:09 +0100

    added read, update and delete actions

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 
-from quart import g, Quart
+from quart import abort, g, Quart
 from quart_db import QuartDB
 from quart_schema import QuartSchema, validate_request, validate_response
 
@@ -34,3 +34,51 @@ async def createPrescription(data: PrescriptionInput) -> Prescription:
          "is_repeating": data.is_repeating},
   )
   return Prescription(**result)
+
+@dataclass
+class Prescriptions:
+  prescriptions: list[Prescription]
+
+@app.get("/prescriptions/")
+@validate_response(Prescriptions)
+async def get_prescriptions() -> Prescriptions:
+  """Get all prescriptions"""
+  query = """SELECT id, medication_name, dosage, frequency, is_repeating 
+              FROM prescriptions"""
+  prescriptions = [
+    Prescription(**row)
+    async for row in g.connection.iterate(query)
+  ]
+  return Prescriptions(prescriptions=prescriptions)
+
+@app.put("/prescriptions/<int:id>/")
+@validate_request(PrescriptionInput)
+@validate_response(Prescription)
+async def update_prescription(id: int, data: PrescriptionInput) -> Prescription:
+  """Update a prescription"""
+  result = await g.connection.fetch_one(
+    """UPDATE prescriptions
+        SET medication_name = :medication_name,
+            dosage = :dosage,
+            frequency = :frequency,
+            is_repeating = :is_repeating
+            WHERE id = :id
+            RETURNING id, medication_name, dosage, frequency, is_repeating""",
+            {"id": id, 
+             "medication_name": data.medication_name, 
+             "dosage": data.dosage,
+             "frequency": data.dosage,
+             "is_repeating": data.is_repeating},
+  )
+  if result is None:
+    abort(404)
+  return Prescription(**result)
+
+@app.delete("/prescriptions/<int:id>/")
+async def delete_prescription(id:int) -> str:
+  """Delete a prescription"""
+  await g.connection.execute(
+    """DELETE FROM prescriptions WHERE id = :id""",
+    {"id": id},
+  )
+  return ""
